0dbc77d730c1e876c42821b441cf5748b47fe97e
Иван Глебов <glebov.vanya8@gmail.com>
2021-10-04 19:51:37 +0300

    Adaptive layout, added missing font, extra comments removed

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -233,10 +233,315 @@ export default {
 
 <style>
 
+@media screen and (min-width: 1065px) and (max-width: 1750px) {
+  div {
+    transition: all .7s !important;
+  }
+
+  span {
+    transition: all .7s !important;
+  }
+
+  .code-snippet {
+    transform: translateY(230px) translateX(-100px) scale(0.8);
+    width: 700px;
+    transition: all .7s;
+  }
+
+  .content-wrapper {
+    width: 100vw;
+  }
+
+  .text-wrapper {
+    width: 500px;
+  }
+
+  #block1 .small-wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 80vw;
+  }
+
+  #block1 .svg-wrapper-smart-contract {
+    margin-right: 100px;
+  }
+
+  .text-wrapper ~ div {
+    margin-top: -100px;
+  }
+
+  #block2 {
+    display: flex;
+    flex-direction: row;
+  }
+
+  #block2 .buttons {
+    margin-top: -100px;
+  }
+
+  #block3 {
+    width: 80vw;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  #block3 .coins-and-ideas {
+    margin-top: -100px;
+  }
+
+  #block4 {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 90vw;
+  }
+
+  #block4 .small-wrapper {
+    width: 500px;
+  }
+
+  #block4 .robot-and-git-and-robonomics .robot {
+    margin-right: 200px;
+    margin-top: -100px;
+    transition: all .7s;
+  }
+
+  .robot-and-git-and-robonomics {
+    display: flex;
+    flex-direction: column;
+  }
+
+  #block4 .git-and-robonomics-margins {
+    margin: 0;
+    top: 400px;
+    left: -950px;
+    transition: all .7s;
+  }
+
+  #block5 {
+    width: 1000px;
+    height: 750px;
+    display: flex;
+    flex-direction: row;
+  }
+
+  #block5 :first-child {
+    width: 300px;
+  }
+
+  #block5 .drone-and-margin {
+    margin-top: -80px;
+    margin-left: 150px;
+  }
+}
+
+@media screen and (min-width: 1065px) and (max-width: 1250px) {
+  .slot-wrapper .code-snippet {
+    transform: translateY(270px) scale(0.7);
+    transition: all .7s;
+    /*transform: scale(0.9);*/
+  }
+
+  #block4 .git-and-robonomics-margins {
+    margin: 0;
+    top: 400px;
+    left: -850px;
+    transition: all .7s;
+  }
+
+  #block4 .small-wrapper {
+    width: 500px;
+  }
+}
+
+@media screen and (max-width: 1065px) {
+
+  .wrapper {
+    flex-direction: column;
+    transition: all .7s;
+  }
+
+  .wrapper .steps {
+    margin-top: -50px;
+    min-height: 0;
+    width: 100vw;
+    margin-left: 0;
+    position: fixed;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 40px;
+    transition: all .7s;
+  }
+
+  .inner-wrapper .code-snippet {
+    transition: all .7s;
+    transform: scale(0.5);
+    width: 100vw;
+    right: 70px;
+    margin-top: 200px;
+  }
+  .inner-wrapper .code-snippet .wrapper {
+    width: 650px;
+    height: 450px;
+  }
+
+  .code-snippet .wrapper .header {
+    min-height: 60px;
+    justify-content: center;
+    align-items: center;
+    transition: all .7s;
+  }
+  .code-snippet .wrapper .title {
+    margin: 0;
+    transition: all .7s;
+  }
+
+  .code-snippet .wrapper .buttons {
+    pointer-events: none;
+    opacity: 0;
+    display: none;
+    transform: scale(0);
+    transition: all .7s;
+  }
+
+  .inner-wrapper .content-wrapper {
+    margin-left: 0;
+    transition: all .7s;
+    align-items: center;
+  }
+
+  .inner-wrapper .content-wrapper .filler_1 {
+    transform: scale(0.7);
+    margin: -15% 0 0 -15%;
+    transition: all .7s;
+  }
+
+  .filler_1 .buttons {
+    display: flex;
+    justify-content: flex-start;
+    margin-top: 20px;
+    transition: all .7s;
+  }
+  .filler_1 .small-wrapper {
+    transition: all .7s;
+    width: 80%;
+  }
+
+  .dot {
+    transform: scale(2);
+    transition: all .7s;
+  }
+
+  .dot.dot1, .dot.dot2, .dot.dot3, .dot.dot4, .dot.dot5 {
+    transform: scale(2);
+    transition: all .7s;
+  }
+
+  .lines {
+    display: none;
+    transition: all .7s;
+  }
+
+  #block3.filler_1 .coins-and-ideas {
+    margin-top: 50px;
+    transition: all .7s;
+  }
+
+  #block4 .robot-and-git-and-robonomics {
+    pointer-events: none;
+    margin-left: 50px;
+    margin-top: -20px;
+    transition: all .7s;
+  }
+  #block4 .robot-and-git-and-robonomics .git-and-robonomics-margins {
+    transition: all .7s;
+    left: 0;
+    top: 0;
+  }
+
+  #block5.filler_1 .drone-and-margin {
+    margin-top: 50px;
+    transition: all .7s;
+  }
+
+}
+
 a {
   margin-right: 20px;
   display: block;
   text-decoration: none;
+  color: white;
+}
+
+.inner-wrapper {
+  width: 100vw;
+  z-index: 15;
+  display: flex;
+}
+
+.crossover {
+  display: inline;
+  background-color: transparent;
+  position: relative;
+}
+
+.crossover::before {
+  border-top: 10px solid #6CEEFF;
+  content: "";
+  margin: 0 auto; /* this centers the line to the full width specified */
+  position: absolute; /* positioning must be absolute here, and relative positioning must be applied to the parent */
+  top: 50%;
+  left: 15px;
+  right: 0;
+  bottom: 0;
+  width: 115%;
+  z-index: -1;
+}
+
+.crossover-top {
+  display: inline;
+  background-color: transparent;
+  position: relative;
+}
+
+.crossover-top::before {
+  border-top: 10px solid #6CEEFF;
+  content: "";
+  margin: 0 auto;
+  position: absolute;
+  top: calc(50% - 15px);
+  left: 15px;
+  right: 0;
+  bottom: 0;
+  width: 110%;
+  z-index: -1;
+}
+
+.crossover-bottom {
+  display: inline;
+  background-color: transparent;
+  position: relative;
+}
+
+.crossover-bottom::before {
+  border-top: 10px solid #6CEEFF;
+  content: "";
+  margin: 0 auto; /* this centers the line to the full width specified */
+  position: absolute; /* positioning must be absolute here, and relative positioning must be applied to the parent */
+  top: calc(50% + 5px);
+  left: 15px;
+  right: 0;
+  bottom: 0;
+  width: 125%;
+  z-index: -1;
+}
+
+.coins-and-ideas {
+  margin-top: 200px;
 }
 
 .solo-play-button {
@@ -253,6 +558,35 @@ a {
   margin-top: 4px;
 }
 
+#block2 .buttons a :first-child {
+  margin-right: 100px;
+  transition: .7s all;
+}
+
+.robot-and-git-and-robonomics {
+  display: flex;
+  flex-direction: row;
+}
+
+.robot-and-git-and-robonomics :nth-child(2) {
+  pointer-events: none;
+}
+
+.git-and-robonomics-margins {
+  position: relative;
+  left: 420px;
+  top: 200px;
+}
+
+.margins-robonomics-network-buttons {
+  margin-top: -15px;
+  margin-left: 35px;
+}
+
+.drone-and-margin {
+  margin-top: 150px;
+}
+
 .svg-wrapper-smart-contract {
   transform: scale(1.2);
   margin-left: 90px;
@@ -261,24 +595,22 @@ a {
 
 .filler_1 {
   height: 700px;
-  /*border: 1px solid white;*/
   opacity: 1;
   visibility: visible;
-  transition: all 1s;
+  transition: all .5s;
 }
 
 .hidden {
-  transition: all 1s;
+  transition: all .5s;
   opacity: 0;
-  /*visibility: hidden;*/
+  pointer-events: none;
 }
 
 .filler_1:last-child {
-  margin-bottom: 200px;
+  height: 750px;
 }
 
 .wrapper {
-  margin-top: 70px;
   width: 100vw;
   z-index: 15;
   display: flex;
@@ -287,7 +619,6 @@ a {
 .steps {
   margin-left: 70px;
   width: 100px;
-  /*border: 1px solid black;*/
   min-height: 400px;
   position: fixed;
 
@@ -298,24 +629,23 @@ a {
 }
 
 .content-wrapper {
-  /*border: 1px solid black;*/
   margin-left: 200px;
   width: 650px;
   color: white;
-  font-family: Roboto, sans-serif;
-  font-size: 40pt;
+  font-family: "Yeseva One", sans-serif;
+  font-size: 34pt;
   font-weight: 300;
 }
 
 .small-wrapper {
-  width: 550px;
+  width: 600px;
 }
 
 .code-snippet {
   position: fixed;
-  /*top: */
   margin-top: -100px;
-  right: 150px;
+  right: 100px;
+  transition: all .7s;
 }
 
 .flex-col {
@@ -341,50 +671,41 @@ a {
 .dot2 {
   transform: translateX(10px) translateY(5px);
   transition: all .7s;
-  /*opacity: 0.5;*/
-
 }
 
 .line2 {
   transform: translateX(-5px) rotate(-30deg) translateY(-13px);
   transition: all .7s;
-  /*opacity: 0.5;*/
 }
 
 .dot3 {
   transform: translateX(-10px);
   transition: all .7s;
-  /*opacity: 0.5;*/
 }
 
 .line3 {
   transform: translateX(9px) translateY(-15px) rotate(70deg);
   transition: all .7s;
-  /*opacity: 0.5;*/
 }
 
 .dot4 {
   transform: translateX(10px);
   transition: all .7s;
-  /*opacity: 0.5;*/
 }
 
 .line4 {
   transform: translateY(-10px) rotate(-50deg) translateY(-20px) translateX(5px);
   transition: all .7s;
-  /*opacity: 0.5;*/
 }
 
 .dot5 {
   transform: translateX(-15px);
   transition: all .7s;
-  /*opacity: 0.5;*/
 }
 
 .line5 {
   transform: translateX(15px) rotate(45deg) translateX(-20px) translateY(-15px);
   transition: all .7s;
-  /*opacity: 0.5;*/
 }
 
 </style>
